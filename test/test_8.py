e54234a3c6e268e959b2ec3eff32bc4c98c1fb2d
rouenlee29 <rouen@pulsifi.me>
2021-05-18 15:31:20 +0800

    feat: first commit

---
+++ test/test_8.py
@@ -0,0 +1,25 @@
+# taken from https://github.com/okken/talks/blob/master/2020/pycon_2020/code/test_8.py
+
+# to run these test, run:
+# python -m pytest test/
+# don't just run `pytest`
+
+import pytest
+from src.triangle import triangle_type
+
+many_triangles = [
+    (90, 60, 30, "right"),
+    (100, 40, 40, "obtuse"),
+    (60, 60, 60, "acute"),
+    (0, 0, 0, "invalid"),
+]
+
+
+@pytest.fixture(params=many_triangles)
+def a_triangle(request):
+    return request.param
+
+
+def test_fix(a_triangle):
+    a, b, c, expected = a_triangle
+    assert triangle_type(a, b, c) == expected
